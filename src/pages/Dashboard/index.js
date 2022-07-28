f2b5c03dd5f208710cb3efbc598226c94f005032
Ze-Victor <josevictorjose8@gmail.com>
2022-07-28 10:25:53 -0300

    fix: ajustes finais

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -8,17 +8,15 @@ import { AccountContext } from "../../context/accountContext";
 export const Dashboard = () => {
   const context = useContext(AccountContext);
 
-  let balance = 0
+  let balance = 0;
+  let name = "";
 
   context.accounts.forEach((account, index) => {
-
     if (context.userLogged === account.cpf) {
-
       balance = account.saldo;
+      name = context.user[index].nome;
     }
-
   });
-
 
   const [dataAccountUserLogged, setDataAccountUserLogged] = useState([
     {
@@ -37,11 +35,9 @@ export const Dashboard = () => {
     },
   ]);
 
-
-
   return (
     <div className="containerDashboard">
-      <SideBar />
+      <SideBar cpf={context.userLogged} name={name} />
       <div>
         <Header title="Dashboard" />
         <div className="content">
@@ -55,25 +51,29 @@ export const Dashboard = () => {
               </div>
               <div className="col-sm">
                 <div className="card">
-                    <h5>Fatura</h5>
-                    <h1>R$ {dataAccountUserLogged[0].fatura}</h1>
+                  <h5>Fatura</h5>
+                  <h1>R$ {dataAccountUserLogged[0].fatura}</h1>
                 </div>
               </div>
             </div>
-            <div className="row" style={{marginTop: "30px"}}>
+            <div className="row" style={{ marginTop: "30px" }}>
               <div className="card-md">
                 <h5>Dependentes</h5>
-                {
-                  dataAccountUserLogged[0].dependentes
-                    .map((dependente, key) => {
-                      return (
-                        <div className="row justify-content-start" key={key} >
-                          <div className="col-6" style={{marginBottom: "8px"}}>{dependente.nome}</div>
-                          <div className="col-2" style={{color: "#70E000", fontSize: "20px"}}>R$ {dependente.saldo}</div>
-                        </div>
-                      );
-                    })
-                }
+                {dataAccountUserLogged[0].dependentes.map((dependente, key) => {
+                  return (
+                    <div className="row justify-content-start" key={key}>
+                      <div className="col-6" style={{ marginBottom: "8px" }}>
+                        {dependente.nome}
+                      </div>
+                      <div
+                        className="col-2"
+                        style={{ color: "#70E000", fontSize: "20px" }}
+                      >
+                        R$ {dependente.saldo}
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
