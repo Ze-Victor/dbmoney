32a0e7fb191ade2c46102e4e92e7caf34d96622e
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-07-28 01:09:27 -0300

    feat: adicionando validação de campos

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -3,3 +3,17 @@ export const changeField = (e, setValue, valueInput) => {
     auxValues[e.target.name] = e.target.value;
     setValue(auxValues);
 };
+
+export const required = (value) => {
+    if (value === undefined || value.toString().trim().length === 0) {
+      return 'Este campo é obrigatório';
+    }
+    return null;
+};
+
+export const isEquals = (value1, value2) => {
+    if (value1 !== value2) {
+        return 'Senha não coincidem';
+    }
+    return null;
+}
