469ac0c08a7a7f5892eafe40761dcb980903fb22
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-07-27 21:35:34 -0300

    fix: ajustes

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -9,15 +9,43 @@ export const Dashboard = () => {
             <SideBar />
             <div>
                 <Header title="Dashboard" />
-                <div className="container">
-                <div class="row">
-                    <div class="col-sm">
-                    Uma de três colunas
+                <div className="content">
+                    <div style={{width: 'fit-content'}}>
+                    <div className="row justify-content-between">
+                        <div className="col-sm">
+                            <div className="card">
+                                Saldo
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                            <div className="card">
+                                <div style={{width: 'fit-content'}}>
+                                    Fatura
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-sm">
-                    Uma de três colunas
+                    <div className="row">
+                        <div className="card">
+                            Dependentes
+                            <div className="row">
+                                <div className="col-8">Nome da pessoa</div>
+                                <div className="col-4">Saldo</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <div className="row">
+                        <div className="card">
+                            Últimos 30 dias
+                            <div className="row">
+                                <div className="col-6">Nome da pessoa</div>
+                                <div className="col-2">Transação</div>
+                                <div className="col-2">Valor</div>
+                                <div className="col-2">Data</div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
